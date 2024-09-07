dda95b0b7c83c87db0dc96a71b4f8284117303fe
aidanb3765 <aidan.blaney@temple.edu>
2024-09-07 15:41:18 -0400

    Creating github repository and updating with the class containing shape and square

---
+++ app/src/main/java/com/example/assignment_0_3515/Shape.kt
@@ -0,0 +1,12 @@
+package com.example.assignment_0_3515
+
+abstract class Shape (_name : String): Dimensionable {
+    var name = _name
+    fun getArea(): Double {
+        return 0.0;
+    }
+}
+
+interface Dimensionable {
+    fun printDimensions()
+}
