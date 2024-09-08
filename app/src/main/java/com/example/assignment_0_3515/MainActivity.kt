00e548f04b9d5dd2ebf88b0681a3083e8808b369
aidanb3765 <aidan.blaney@temple.edu>
2024-09-08 19:28:20 -0400

    Added the main functions, and attempted to try and create the application but ran out of time

--- app/src/main/java/com/example/assignment_0_3515/MainActivity.kt
+++ app/src/main/java/com/example/assignment_0_3515/MainActivity.kt
@@ -9,5 +9,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    fun main(){
+        val shape = arrayOf( Square("Square"), Circle("Circle"), Triangle("Triangle"), EquilateralTri("Equilateral Triangle"))
 
+        for (s in shape) {
+            if (s is Square) {
+                println("Height and length for SQUARE:")
+                val height = readLine()!!.toDouble()
+                val length = readLine()!!.toDouble()
+                s.setDimensions(height, length)
+            }
+            else if (s is Circle) {
+                println("Radius for CIRCLE:")
+                val radius = readLine()!!.toDouble()
+                s.setDimensions(radius)
+            }
+            else if (s is Triangle) {
+                println("Each side of the TRIANGLE:")
+                val sideA = readLine()!!.toDouble()
+                val sideB = readLine()!!.toDouble()
+                val sideC = readLine()!!.toDouble()
+                s.setDimensions(sideA, sideB, sideC)
+            }
+            else if (s is EquilateralTri) {
+                println("Side of EQUILATERAL TRIANGLE:")
+                val side = readLine()!!.toDouble()
+                s.setDimensions(side)
+            }
+        }
+
+        for (s in shape) {
+            println("Shape name: ${s.name}")
+            s.printDimensions()
+            println("Area of shape: ${s.getArea()}")
+            println()
+        }
+    }
 }
--- app/src/main/java/com/example/assignment_0_3515/Square.kt
+++ app/src/main/java/com/example/assignment_0_3515/Square.kt
@@ -4,7 +4,7 @@ class Square (_name: String) : Shape(_name){
     var height: Double = 0.0
     var length: Double = 0.0
 
-    fun setDimensions (height: Double, length: Double) {
+    fun setDimensions(height: Double, length: Double) {
         this.height = height
         this.length = length
     }
